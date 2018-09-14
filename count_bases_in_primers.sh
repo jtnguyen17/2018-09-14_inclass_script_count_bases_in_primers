e6817dc1f3426b0a17de287848b4eda310e87a61
Jessica Nguyen <jtnguyen17@dons.usfca.edu>
2018-09-14 15:04:59 -0700

    Initial draft of primer base counting script

---
+++ count_bases_in_primers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# A script to count the number of bases in a primer
+# it expects any number of fasta files as input
+# it will be wrong if the last line of sequence info
+# does not have a newline character
+
+# expecting a fasta file formatted like this:
+
+# > sequence_id
+# AGCTCGTC
+
+# this will get the second line (the bases) in the files
+# that's the head piped into tail
+# then count the characters (the wc -m)
+# but then also subtract one because wc -m counts newlines!
+# sigh
+expr $(head -2 primer_B.fasta | tail -1 | wc -m) -1
+
+# Another solution would be to use tr like so
+# head -2 primer_B.fasta | tail -1 | tr -d '/n' | wc -m
